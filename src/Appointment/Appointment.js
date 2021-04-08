0285b5881da6e476fa09e32634c0fe3fccacab62
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-08 18:35:45 +0000

    imported ArrowRight, Calender, Clock, Loaction, renamed classNames

--- src/Appointment/Appointment.js
+++ src/Appointment/Appointment.js
@@ -1,21 +1,43 @@
 import React from "react";
+import ArrowRight from "../Components/arrow-right";
+import Calender from "../Components/calender";
+import Clock from "../Components/clock";
+import Location from "../Components/location";
 import "./appointment.css";
+
+let size = 17;
 
 const Appointment = () => {
   return (
-    <div className="appointment_wrapper">
-      <div className="appointment_header">
-        <div className="appointment_name ">Name</div>
+    <div className="wrapper">
+      <div className="header">
 
-        <div className="appointment_chevonRight">Icon</div>
+        <div className="header_text ">Moon 263-2</div>
+        <ArrowRight width={size} height={size} />
+
       </div>
 
-      <div className="appointment_body">
-        <div className="appointment_item">Name</div>
+      <div className="body">
+        <div className="body_item">
+          <Calender width={size} height={size} />
+          <div className="margin-spacer-right" />
 
-        <div className="appointment_item">Icon</div>
+          <div className="body_text"> April 4, 2019</div>
+        </div>
 
-        <div className="appointment_item">Icon</div>
+        <div className="body_item">
+          <Clock width={size} height={size} />
+          <div className="margin-spacer-right" />
+
+          <div className="body_text"> 6:00 am - 6:30 am</div>
+        </div>
+
+        <div className="body_item">
+          <Location width={size} height={size} color="#ff2f00" />
+          <div className="margin-spacer-right" />
+
+          <div className="body_text   colored"> Capilla St 103</div>
+        </div>
       </div>
     </div>
   );
