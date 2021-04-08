8660e78b763b05effee9a7edca2612900f8aabca
Sam-2019 <49334378+Sam-2019@users.noreply.github.com>
2021-04-08 03:11:42 +0000

    added img url

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -5,7 +5,14 @@ const Contact = () => {
   return (
     <div className="contact_wrapper">
       <div className="contact_header">
-        <div className="contact_header_image">Pic</div>
+        <div className="contact_header_image">
+          Pic
+          <img
+            src="https://c.pxhere.com/photos/e7/a6/business_woman_business_man_business_businessman_businesswoman_businesslady_business_lady_lady-1278242.jpg!d"
+            alt="user"
+            className="user_image"
+          />
+        </div>
 
         <div className="contact_header_profile">
           <div className="contact_header_profile_name">Kate Lariscy Branch</div>
